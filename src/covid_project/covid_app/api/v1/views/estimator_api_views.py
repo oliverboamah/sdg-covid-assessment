282f176a817e5c43a2325bd71c3e9f39500a18d1
codingoliver <oliverboamah@yahoo.com>
2020-04-18 17:47:30 +0000

    Add alternative estimator endpoint which returns XML

--- src/covid_project/covid_app/api/v1/views/estimator_api_views.py
+++ src/covid_project/covid_app/api/v1/views/estimator_api_views.py
@@ -4,14 +4,15 @@ from rest_framework import status
 from rest_framework.response import Response
 
 # covid_app imports
+from rest_framework_xml.renderers import XMLRenderer
+
 from src.covid_project.covid_app.api.v1.serializers.estimator_serializer import EstimatorSerializer
 
 # import estimator.py
 from src import estimator
 
 
-class EstimatorJSONAPIView(APIView):
-
+class EstimatorAPIView(APIView):
     def post(self, request):
         try:
             estimator_serializer = EstimatorSerializer(data=request.data)
@@ -27,3 +28,11 @@ class EstimatorJSONAPIView(APIView):
                 'message': e.__str__()
             }
             return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EstimatorJSONAPIView(EstimatorAPIView):
+    pass
+
+
+class EstimatorXMLAPIView(EstimatorAPIView):
+    renderer_classes = (XMLRenderer,)
--- src/covid_project/covid_app/urls.py
+++ src/covid_project/covid_app/urls.py
@@ -2,10 +2,13 @@
 from django.urls import path
 
 # covid_app imports
-from src.covid_project.covid_app.api.v1.views.estimator_api_json_view import EstimatorJSONAPIView
+from src.covid_project.covid_app.api.v1.views.estimator_api_views import (
+    EstimatorJSONAPIView, EstimatorXMLAPIView
+)
 
 app_name = 'covid_app'
 urlpatterns = [
     path(route='api/v1/on-covid-19', view=EstimatorJSONAPIView.as_view(), name='default_estimator'),
     path(route='api/v1/on-covid-19/json', view=EstimatorJSONAPIView.as_view(), name='estimator_json'),
+    path(route='api/v1/on-covid-19/xml', view=EstimatorXMLAPIView.as_view(), name='estimator_xml'),
 ]
