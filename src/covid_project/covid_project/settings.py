7e90819c8d48cd3f0287991e1307309394632312
codingoliver <oliverboamah@yahoo.com>
2020-04-18 15:33:56 +0000

    Add STATIC_ROOT to settings.py

--- src/covid_project/covid_project/settings.py
+++ src/covid_project/covid_project/settings.py
@@ -114,3 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# Project directory
+PROJECT_DIR = os.path.dirname(__file__)
+
+#
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static_media/')
